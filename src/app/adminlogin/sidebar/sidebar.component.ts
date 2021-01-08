aefc9bc5a9029a496cd3dc52dc49b92986a4afae
sitcomdeveloper <sitcomdeveloper43@gmail.com>
2021-01-08 18:44:05 +0530

    Rohan latest code

--- src/app/adminlogin/sidebar/sidebar.component.ts
+++ src/app/adminlogin/sidebar/sidebar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { timeStamp } from 'console';
 import { ApiService } from 'src/app/api.service';
 
 @Component({
